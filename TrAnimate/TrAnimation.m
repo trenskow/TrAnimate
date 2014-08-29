ff395d4786d603a7cbcec1264a50bb8ff88b8866
Kristian Trenskow <trenskow@me.com>
2014-08-29 18:16:37 +0200

    Not used.

--- TrAnimate/TrAnimation.m
+++
@@ -1,9 +0,0 @@
-//
-//  TrAnimation.m
-//  TrAnimate
-//
-//  Created by Kristian Trenskow on 29/08/14.
-//  Copyright (c) 2014 Kristian Trenskow. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
