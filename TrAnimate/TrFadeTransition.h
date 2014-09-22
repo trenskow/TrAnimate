057928a5ebc5756479770dee3b66e044dc5f8d14
Kristian Trenskow <trenskow@me.com>
2014-09-22 22:41:04 +0200

    Added TrFadeTransition.

--- TrAnimate/TrFadeTransition.h
+++ TrAnimate/TrFadeTransition.h
@@ -1,20 +1,20 @@
 //
-//  TrAnimate.h
+//  TrPushTransition.h
 //  TrAnimate
 //
 //  Copyright (c) 2013-2014, Kristian Trenskow
 //  All rights reserved.
-//  
+//
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
-//  
+//
 //  1. Redistributions of source code must retain the above copyright notice,
 //  this list of conditions and the following disclaimer.
-//  
+//
 //  2. Redistributions in binary form must reproduce the above copyright notice,
 //  this list of conditions and the following disclaimer in the documentation
 //  and/or other materials provided with the distribution.
-//  
+//
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 //  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 //  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,43 +28,34 @@
 //  POSSIBILITY OF SUCH DAMAGE.
 //
 
-@import Foundation;
-
-FOUNDATION_EXPORT double TrAnimateVersionNumber;
-FOUNDATION_EXPORT const unsigned char TrAnimateVersionString[];
-
-#import "TrInterpolatable.h"
-
-#import "NSNumber+TrAnimateAdditions.h"
-#import "NSValue+TrAnimateAdditions.h"
-#import "UIColor+TrAnimateAdditions.h"
-
-#import "TrAnimatable.h"
-
-#import "UIView+TrAnimateAdditions.h"
-#import "CALayer+TrAnimateAdditions.h"
-
-#import "TrCurve.h"
-
-#import "TrAnimation.h"
-
-#import "TrLayerAnimation.h"
-
-#import "TrOpacityAnimation.h"
-#import "TrFadeAnimation.h"
-#import "UIView+TrFadeAnimationAdditions.h"
-#import "TrPositionAnimation.h"
-#import "TrMoveAnimation.h"
-#import "UIView+TrMoveAnimationAdditions.h"
-#import "TrScaleAnimation.h"
-#import "TrPopInAnimation.h"
-#import "UIView+TrPopInAnimationAdditions.h"
-#import "TrRotateAnimation.h"
-
-#import "TrDirectAnimation.h"
-#import "NSObject+TrDirectAnimationAdditions.h"
+@import UIKit;
 
 #import "TrAnimationGroup.h"
 
-#import "TrFadeTransition.h"
-#import "TrPushTransition.h"
+@class TrCurve;
+
+@interface TrFadeTransition : TrAnimationGroup
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration
+                         delay:(NSTimeInterval)delay
+                         curve:(TrCurve *)curve
+                    completion:(void (^)(BOOL finished))completion;
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration
+                         delay:(NSTimeInterval)delay
+                         curve:(TrCurve *)curve;
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration
+                         delay:(NSTimeInterval)delay;
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration;
+
+@end
---
+++ TrAnimate/TrFadeTransition.m
@@ -0,0 +1,117 @@
+//
+//  TrPushTransition.h
+//  TrAnimate
+//
+//  Copyright (c) 2013-2014, Kristian Trenskow
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//  this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "UIView+TrAnimateAdditions.h"
+
+#import "TrFadeAnimation.h"
+
+#import "TrFadeTransition.h"
+
+@implementation TrFadeTransition
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration
+                         delay:(NSTimeInterval)delay
+                         curve:(TrCurve *)curve
+                    completion:(void (^)(BOOL finished))completion {
+    
+    toView.frame = fromView.frame;
+    toView.alpha = .0;
+    toView.hidden = NO;
+    
+    [fromView.superview addSubview:toView];
+    
+    TrFadeTransition *animationGroup = [self animationGroupWithCompletion:completion];
+    
+    if (!fromView.opaque)
+        [animationGroup addAnimation:[TrFadeAnimation animate:fromView
+                                                     duration:duration
+                                                        delay:delay
+                                                    direction:TrFadeAnimationDirectionOut
+                                                        curve:curve
+                                                   completion:nil]];
+    
+    [animationGroup addAnimation:[TrFadeAnimation animate:toView
+                                                 duration:duration
+                                                    delay:delay
+                                                direction:TrFadeAnimationDirectionIn
+                                                    curve:curve
+                                               completion:^(BOOL finished) {
+                                                   [fromView removeFromSuperview];
+                                               }]];
+    
+    return animationGroup;
+    
+}
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration
+                         delay:(NSTimeInterval)delay
+                         curve:(TrCurve *)curve {
+    
+    return [self transitionFrom:fromView
+                             to:toView
+                       duration:duration
+                          delay:delay
+                          curve:curve
+                     completion:nil];
+    
+}
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration
+                         delay:(NSTimeInterval)delay {
+    
+    return [self transitionFrom:fromView
+                             to:toView
+                       duration:duration
+                          delay:delay
+                          curve:nil
+                     completion:nil];
+    
+}
+
++ (instancetype)transitionFrom:(UIView *)fromView
+                            to:(UIView *)toView
+                      duration:(NSTimeInterval)duration {
+    
+    return [self transitionFrom:fromView
+                             to:toView
+                       duration:duration
+                          delay:.0
+                          curve:nil
+                     completion:nil];
+    
+}
+
+@end
